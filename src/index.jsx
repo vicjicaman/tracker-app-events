34159ab0a564fb92cdb76576918ef6182f5c5e90
Victor Jimenez <vic.jicama@gmail.com>
2019-06-25 15:17:51 +0200

    Separate query, stream, ui and stream packages

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+
+import {EventConnector, EventActionConnector} from './EventConnector';
+import * as Event from './state/actions'
+import {reducers} from './state/reducers';
+
+export {
+  Event,
+  EventConnector,
+  EventActionConnector,
+  reducers
+};
